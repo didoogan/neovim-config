ee8b0f4b11db9f66c8a065a11624ac7ad6dcb43d
Tymoshenko Dmytro <tzd0409@gmail.com>
2026-02-26 14:33:07 +0200

    update config

--- lua/plugins/cspell.lua
+++ lua/plugins/cspell.lua
@@ -1,21 +1,22 @@
 return {
   {
     "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        cspell_ls = {
-          filetypes = {
-            "python", "lua", "javascript", "javascriptreact",
-            "typescript", "typescriptreact", "rust",
-            "markdown", "text", "gitcommit", "json", "yaml",
-          },
-          settings = {
-            cSpell = {
-              language = "en,uk",
-            },
+    init = function()
+      -- cspell_ls uses the new Neovim 0.11 native LSP API (lsp/ directory),
+      -- so it must be configured via vim.lsp.config instead of opts.servers
+      vim.lsp.config("cspell_ls", {
+        filetypes = {
+          "python", "lua", "javascript", "javascriptreact",
+          "typescript", "typescriptreact", "rust",
+          "markdown", "text", "gitcommit", "json", "yaml",
+        },
+        settings = {
+          cSpell = {
+            language = "en,uk",
           },
         },
-      },
-    },
+      })
+      vim.lsp.enable("cspell_ls")
+    end,
   },
 }
